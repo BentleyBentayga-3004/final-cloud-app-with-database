cbe5f608033852529631dde9b9d0e2207d1491f4
BentleyBentayga-3004 <minhhuuy300405@gmail.com>
2026-08-11 06:02:04 +0000

    Fix admin.E202 error

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,30 +1,30 @@
 from django.contrib import admin
-# Import 7 classes từ file models.py của bạn
 from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 
 # Khai báo các lớp Inline
 class ChoiceInline(admin.StackedInline):
     model = Choice
-    extra = 4 # Hiển thị sẵn 4 ô trống để nhập 4 đáp án (A, B, C, D)
+    extra = 4
 
 class QuestionInline(admin.StackedInline):
     model = Question
-    extra = 5 # Hiển thị sẵn 5 ô trống để nhập câu hỏi
+    extra = 5
 
-# Nhúng Choice vào Question Admin
+# Nhúng Choice vào Question
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
     list_display = ['question_text', 'course']
 
-# Nhúng Question vào Lesson Admin
+# KHÔNG nhúng Question vào Lesson nữa
 class LessonAdmin(admin.ModelAdmin):
-    inlines = [QuestionInline]
     list_display = ['title', 'course']
 
+# Nhúng Question vào Course
 class CourseAdmin(admin.ModelAdmin):
+    inlines = [QuestionInline]
     list_display = ('name', 'pub_date')
 
-# Đăng ký các model để chúng xuất hiện trên giao diện Admin
+# Đăng ký các model
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Question, QuestionAdmin)
